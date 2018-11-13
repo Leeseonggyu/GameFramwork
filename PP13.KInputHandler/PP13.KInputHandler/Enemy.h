961775e1de73d2cdb35a156a430fc67fb23a8e5c
이성규 <seong9945@naver.com>
2018-11-13 21:02:31 +0900

    PP13.KInputHandler 11월 13일

--- PP13.KInputHandler/PP13.KInputHandler/Enemy.h
+++ PP13.KInputHandler/PP13.KInputHandler/Enemy.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "SDLGameObject.h"
+
+class Enemy : public SDLGameObject
+{
+public:
+	Enemy(const LoaderParams* pParams);
+	virtual void draw();
+	virtual void update();
+	virtual void clean();
+};
